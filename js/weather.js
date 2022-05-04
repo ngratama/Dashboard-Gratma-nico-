f798c918e2d7b1053b6815f10a19f572b58bb990
ngratama <95258322+ngratama@users.noreply.github.com>
2022-05-04 14:53:24 -0700

    added more favicon and other api
    added api and faicon

--- js/weather.js
+++ js/weather.js
@@ -10,24 +10,25 @@ const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${A
 
 // Using fetch to get data
 fetch(url)
-.then( response => response.json() )
-.then( data => {
+  .then(response => response.json())
+  .then(data => {
 
-  // Check-check: Is data good? 
-  console.log( data );
-  
-  // Get Container for Weather   
-  const weatherContainer = document.querySelector('.weather');
-  
-  // Template to output
-  const template = `
+    // Check-check: Is data good? 
+    console.log(data);
+
+    // Get Container for Weather   
+    const weatherContainer = document.querySelector('.weather');
+
+    // Template to output
+    const template = `
     <h1>Weather</h1>
     <data value="${data.name}" class="city">${data.name}</data>
     <data value="${data.main.temp}" class="temp">${data.main.temp}&#8457;</data>
+    <data value="${data.main.humidity}" class="humidity">${data.main.humidity}</data>
     <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Placeholder">
   `;
-  
-  // Insert dynamic template to container
-  weatherContainer.insertAdjacentHTML("afterbegin", template);
-  
-});
+
+    // Insert dynamic template to container
+    weatherContainer.insertAdjacentHTML("afterbegin", template);
+
+  });
